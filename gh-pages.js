e8eeae974dd68a0f65e016abaac7333c659a8a8d
pig3629 <pig3629@gmail.com>
2022-10-30 23:09:34 +0800

    OK

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/pig3629/my_runes.git', // Update to point to your repository
+        repo: 'https://github.com/pig3629/my_runes2.git', // Update to point to your repository
         user: {
             name: 'pig3629', // update to use your name
             email: 'pig3629@gmail.com' // Update to use your email
